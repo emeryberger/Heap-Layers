349e317c2cc85f9d6eb161a6c471086917690756
Emery Berger <emery.berger@gmail.com>
2012-07-18 14:26:03 +0200

    Moved everything into HL namespace; canonicalized value to VALUE.

--- utility/gcd.h
+++ utility/gcd.h
@@ -1,15 +1,29 @@
-#ifndef GCD_H
-#define GCD_H
+// -*- C++ -*-
 
-template <int a, int b> struct gcd
-{
-  static const int value = gcd<b, a%b>::value;
-};
+#ifndef HL_GCD_H
+#define HL_GCD_H
 
-template <int a> struct gcd<a, 0>
-{
-  static const int value = a;
-};
+namespace HL {
 
+  template <int a, int b>
+  class gcd;
+  
+  template <int a> 
+  class gcd<a, 0>
+  {
+  public:
+    enum { VALUE = a };
+    static const int value = a;
+  };
+  
+  template <int a, int b>
+  class gcd
+  {
+  public:
+    enum { VALUE = gcd<b, a%b>::VALUE };
+    static const int value = gcd<b, a%b>::value;
+  };
+  
+}
 
 #endif
--- utility/lcm.h
+++ utility/lcm.h
@@ -1,11 +1,23 @@
-#ifndef LCM_H
-#define LCM_H
+// -*- C++ -*-
+
+#ifndef HL_LCM_H
+#define HL_LCM_H
 
 #include "gcd.h"
 
-template <int a, int b> struct lcm
-{
-  static const int value = (a * b) / (gcd<a, b>::value);
-};
+namespace HL {
+
+  template <int a, int b>
+  class lcm;
+
+  template <int a, int b>
+  class lcm
+  {
+  public:
+    enum { VALUE = (a * b) / (gcd<a, b>::VALUE) };
+    static const int value = (a * b) / (gcd<a, b>::value);
+  };
+
+}
 
 #endif
