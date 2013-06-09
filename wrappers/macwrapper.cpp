c9fbc0b8f8eb30906ec25e58ee71a997e4213bb7
emeryberger <emery@cs.umass.edu>
2013-06-09 14:19:54 -0400

    Added Hoard message (currently disabled).

--- wrappers/macwrapper.cpp
+++ wrappers/macwrapper.cpp
@@ -17,6 +17,8 @@
 #include <malloc/malloc.h>
 #include <errno.h>
 
+#include <unistd.h>
+
 /*
   To use this library,
   you only need to define the following allocation functions:
@@ -488,6 +490,10 @@ public:
     theDefaultZone.memalign     = MACWRAPPER_PREFIX(malloc_zone_memalign);
     theDefaultZone.free_definite_size = MACWRAPPER_PREFIX(malloc_zone_free_definite_size);
     theDefaultZone.pressure_relief = NULL;
+#if 0
+    char str[] = "Using the Hoard scalable memory manager (http://www.hoard.org).\n";
+    write (2, str, strlen(str));
+#endif
     malloc_zone_register (&theDefaultZone);
   }
 };
