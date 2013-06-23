bf978396b7a80b905e2f75c903b44362c403c3b2
emeryberger <emery@cs.umass.edu>
2013-06-22 18:03:00 -0700

    Added a unit test.

---
+++ utility/ilog2.cpp
@@ -0,0 +1,19 @@
+#include "ilog2.h"
+
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+int
+main()
+{
+  for (int i = 1; i < 1048577; i++) {
+    int l2 = HL::ilog2(i);
+    // Make sure that 2^ceil(log2(i)) is at least i.
+    assert ((1 << l2) >= i);
+    if (l2 > 0) {
+      assert (HL::ilog2(1 << l2-1) + 1 == l2);
+    }
+  }
+  return 0;
+}
