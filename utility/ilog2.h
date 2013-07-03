ee21f8ba88a35bec6fdd5ea3e371fa155f7b432d
emeryberger <emery@cs.umass.edu>
2013-07-03 10:03:57 -0400

    Removed cruft, since x86-64 technically doesn't support inline asm.

--- utility/ilog2.h
+++ utility/ilog2.h
@@ -28,13 +28,6 @@ namespace HL {
     asm ("bsrl %0, %0" : "=r" (sz) : "0" (sz));
     return (int) sz;
   }
-#elif 0 // defined(__GNUC__) && defined(__x86_64__)
-  static inline int ilog2 (size_t sz)
-  {
-    sz = (sz << 1) - 1;
-    asm ("bsrq %0, %0" : "=r" (sz) : "0" (sz));
-    return (int) sz;
-  }
 #elif defined(__GNUC__)
   // Just use the intrinsic.
   static inline int ilog2 (size_t sz)
