3e85d10b29f66644cbe1cb18fe6f0f8eaa5edd01
emeryberger <emery@cs.umass.edu>
2013-10-14 10:30:16 -0400

    Cleanup and comments added.

--- wrappers/x86jump.h
+++ wrappers/x86jump.h
@@ -1,6 +1,20 @@
 #ifndef X86JUMP_H
 #define X86JUMP_H
 
+/**
+
+   Jump code to permit detouring of functions.
+   Use by instantiating into the original start of the function
+   as follows:
+
+   new (originalFunctionStart) x86Jump (newFunctionStart);
+
+   Based on code contributed by Charlie Curtsinger <charlie@cs.umass.edu>
+
+   @author Emery Berger <emery@cs.umass.edu>
+
+ **/
+
 #include <new>
 #include <stdint.h>
 
@@ -13,13 +27,15 @@
 
 PACK(
 struct X86Jump32 {
-    volatile uint8_t jmp_opcode;
-    volatile uint32_t jmp_offset;
-
-    X86Jump32(void *target) {
-        jmp_opcode = 0xE9;
-        jmp_offset = (uint32_t)((intptr_t)target - (intptr_t)this) - sizeof(struct X86Jump32);
-    }
+  volatile uint8_t jmp_opcode;
+  volatile uint32_t jmp_offset;
+  
+  // Install a direct 32-bit jump.
+  X86Jump32 (void *target)
+  {
+    jmp_opcode = 0xE9;
+    jmp_offset = (uint32_t)((intptr_t)target - (intptr_t)this) - sizeof(struct X86Jump32);
+  }
 });
 
 PACK(
@@ -29,14 +45,15 @@ struct X86Jump64 {
   volatile uint32_t offset;
   volatile uint64_t addr;
 
-    X86Jump64(void *target) {
-      //FF 25 00 00 00 00
-      // target
-      farjmp = 0x25ff;
-      offset = 0x00000000;
-      addr = (uint64_t) target;
-    }
+  // Install a 64-bit jump (a relative jump to the target with a 0 offset).
+  X86Jump64 (void *target)
+  {
+    farjmp = 0x25ff;
+    offset = 0x00000000;
+    addr   = (uint64_t) target;
+  }
 });
+
 
 PACK(
 struct X86_64Jump {
@@ -45,12 +62,16 @@ struct X86_64Jump {
         uint8_t jmp64[sizeof(X86Jump64)];
     };
     
-    X86_64Jump(void *target) {
-        if((uintptr_t)target - (uintptr_t)this <= 0x00000000FFFFFFFFu || (uintptr_t)this - (uintptr_t)target <= 0x00000000FFFFFFFFu) {
-            new(this) X86Jump32(target);
-        } else {
-            new(this) X86Jump64(target);
-        }
+    X86_64Jump (void *target) {
+      // When the target is close enough, use a 32-bit jump;
+      // otherwise, use the full 64-bit jump.
+      if ((uintptr_t)target - (uintptr_t)this <= 0x00000000FFFFFFFFu ||
+	  (uintptr_t)this - (uintptr_t)target <= 0x00000000FFFFFFFFu) 
+	{
+	  new (this) X86Jump32(target);
+	} else {
+	new (this) X86Jump64(target);
+      }
     }
 });
 
