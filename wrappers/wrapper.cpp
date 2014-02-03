33d60a65a4dba2b487d8b7ae3107ed0a8db22a30
Emery Berger <emery.berger@gmail.com>
2014-02-02 19:58:59 -0500

    More FreeBSD compatibility improvements.

--- wrappers/wrapper.cpp
+++ wrappers/wrapper.cpp
@@ -150,7 +150,10 @@ extern "C" void * MYCDECL CUSTOM_CALLOC(size_t nelem, size_t elsize)
 #if !defined(_WIN32)
 extern "C" void * MYCDECL CUSTOM_MEMALIGN (size_t alignment, size_t size);
 
-extern "C" int CUSTOM_POSIX_MEMALIGN (void **memptr, size_t alignment, size_t size) throw()
+extern "C" int CUSTOM_POSIX_MEMALIGN (void **memptr, size_t alignment, size_t size)
+#if !defined(__FreeBSD__)
+throw()
+#endif
 {
   // Check for non power-of-two alignment.
   if ((alignment == 0) ||
@@ -305,7 +308,7 @@ extern "C" int CUSTOM_MALLOC_SET_STATE (void * ptr) {
   return 0; // success.
 }
 
-#if defined(__GNUC__)
+#if defined(__GNUC__) && !defined(__FreeBSD__)
 extern "C" struct mallinfo CUSTOM_MALLINFO(void) {
   // For now, we return useless stats.
   struct mallinfo m;
