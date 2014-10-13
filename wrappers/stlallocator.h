63dd402bc2f20b8999d79f905b4222056743b136
Charlie Curtsinger <charlie@cs.umass.edu>
2014-10-13 18:03:42 -0400

    Changes to STL allocator type from C++11

--- wrappers/stlallocator.h
+++ wrappers/stlallocator.h
@@ -134,9 +134,30 @@ public:
     Super::free (p);
   }
   
+#if __cplusplus >= 201103L  
+  // Updated API from C++11 to work with move constructors
+  template<class U, class... Args>
+  void construct (U* p, Args&&... args) {
+    new((void *) p) U(std::forward<Args>(args)...);
+  }
+  
+  template<class U>
+  void destroy (U* p) {
+    p->~U();
+  }
+
+#else
+  // Legacy API for pre-C++11
   void construct (pointer p, const T& val) { 
     new ((void *) p) T (val);
   }
+  
+  void destroy (pointer p) {
+    ((T*)p)->~T();
+  }
+  
+#endif
+
   void destroy (pointer p) {
     p->~T();
   }
