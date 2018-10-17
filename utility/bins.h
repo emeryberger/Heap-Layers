c6f9004f8a8feabf35a45a85905472ba52bdc496
emeryberger <emery.berger@gmail.com>
2018-10-17 14:14:45 -0700

    Stick with half size for now, just for symmetry with others. Should really be parameterized.

--- utility/bins.h
+++ utility/bins.h
@@ -63,7 +63,7 @@ namespace HL {
 
   public:
 
-    enum { BIG_OBJECT = Size - sizeof(Header) };
+    enum { BIG_OBJECT = Size / 2 - sizeof(Header) };
     enum { NUM_BINS   = HL::ilog2(Size) - HL::ilog2(sizeof(max_align_t)) + 1 };
 
     static inline constexpr int getSizeClass (size_t sz) {
