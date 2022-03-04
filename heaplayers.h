69f3f15d10e44876a9b85bbd23e5f63ddcc5b05e
Emery Berger <emery.berger@gmail.com>
2022-03-04 17:41:55 -0500

    Revert "Added support for redefining realloc"

--- heaplayers.h
+++ heaplayers.h
@@ -93,10 +93,6 @@ namespace HL {}
 #pragma warning( disable:4786 4512 )
 #endif
 
-#if !defined(HL_USE_XXREALLOC)
-#define HL_USE_XXREALLOC 0
-#endif
-
 #include "utility/all.h"
 #include "heaps/all.h"
 #include "locks/all.h"
