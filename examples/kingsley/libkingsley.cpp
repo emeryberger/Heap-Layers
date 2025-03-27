e9f0af0e8a2b248358095a1ce0ff090516f81d31
Emery Berger <emery.berger@gmail.com>
2025-03-27 20:42:53 +0100

    Added wrappers.

--- examples/kingsley/libkingsley.cpp
+++ examples/kingsley/libkingsley.cpp
@@ -54,5 +54,47 @@ inline static TheCustomHeapType * getCustomHeap() {
 #pragma warning(disable:4273)
 #endif
 
-//#include "wrappers/wrapper.cpp"
+#include "printf.h"
+
+#if !defined(_WIN32)
+#include <unistd.h>
+
+extern "C" {
+  // For use by the replacement printf routines (see
+  // https://github.com/emeryberger/printf)
+  void _putchar(char ch) { ::write(1, (void *)&ch, 1); }
+}
+#endif
+
+#include "wrappers/generic-memalign.cpp"
+
+extern "C" {
+  
+  void * xxmalloc (size_t sz) {
+    auto ptr = getCustomHeap()->malloc (sz);
+    // printf_("xxmalloc %lu = %p\n", sz, ptr);
+    return ptr;
+  }
+
+  void xxfree (void * ptr) {
+    getCustomHeap()->free (ptr);
+  }
+
+  void * xxmemalign(size_t alignment, size_t sz) {
+    return generic_xxmemalign(alignment, sz);
+  }
+  
+  size_t xxmalloc_usable_size (void * ptr) {
+    return getCustomHeap()->getSize (ptr);
+  }
+
+  void xxmalloc_lock() {
+    // getCustomHeap()->lock();
+  }
+
+  void xxmalloc_unlock() {
+    // getCustomHeap()->unlock();
+  }
+  
+}
 
